e1e770a673657b42f4509f6662183cd1effc3c9c
LCDKKJ <koovex@gmail.com>
2017-04-15 17:26:41 +0200

    v2.1.1.

--- Compare/compare/Main.java
+++ Compare/compare/Main.java
@@ -4,78 +4,77 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class Main{
+public class Main {
 
 	public static void main(String[] args) throws IOException {
 		
 		switch (args[0]) {
 		
 		case "0":
-			compare("Inicializálások.txt");
-			break;
-			
+			compare("0_Inicializálások.txt");
+			break;			
 		case "1":
-			compare("Új játék kezdése.txt");
+			compare("1_Czirjak.txt");
 			break;
 		case "2":
-			compare("Új vonat indítása.txt");
+			compare("2_Új vonat indítása.txt");
 			break;
 		case "3":
-			compare("Vonat mozgása.txt");
+			compare("3_Vonat mozgása.txt");
 			break;
 		case "4":
-			compare("Alagút építés.txt");
+			compare("4_Alagút építés.txt");
 			break;
 		case "5":
-			compare("Alagút lebontás.txt");
+			compare("5_Alagút lebontás.txt");
 			break;
 		case "6":
-			compare("Alagút belépés-kilépés.txt");
+			compare("6_Alagút belépés-kilépés.txt");
 			break;
 		case "7":
-			compare("Váltó átállítása.txt");
+			compare("7_Váltó átállítása.txt");
 			break;
 		case "8":
-			compare("Kisiklás a váltón.txt");
+			compare("8_Kisiklás a váltón.txt");
 			break;
 		case "9":
-			compare("Állomás aktiválása.txt");
+			compare("9_Állomás aktiválása.txt");
 			break;
 		case "10":
-			compare("Utasak leszállása(+ szeneskocsi helye mûködése).txt");
+			compare("10_Utasak leszállása(+ szeneskocsi helye mûködése).txt");
 			break;
 		case "11":
-			compare("Utasok felszállása.txt");
+			compare("11_Utasok felszállása.txt");
 			break;
 		case "12":
-			compare("Utas nem száll le.txt");
+			compare("12_Utas nem száll le.txt");
 			break;
 		case "13":
-			compare("Keresztezõdésen áthaladás vízszintesen.txt");
+			compare("13_Keresztezõdésen áthaladás vízszintesen.txt");
 			break;
 		case "14":
-			compare("Keresztezõdésen áthaladás függõlegesen.txt");
+			compare("14_Keresztezõdésen áthaladás függõlegesen.txt");
 			break;
 		case "15":
-			compare("Ütközés ellenõrzés.txt");
+			compare("15_Ütközés ellenõrzés.txt");
 			break;
 		case "16":
-			compare("Pálya megnyerése, új pálya inicializálása.txt");
+			compare("16_Pálya megnyerése, új pálya inicializálása.txt");
 			break;
 		case "17":
-			compare("Kilépés.txt");
+			compare("17_Kilépés.txt");
 			break;
 		case "18":
-			compare("Foglalt váltó átállítása.txt");
+			compare("18_Foglalt váltó átállítása.txt");
 			break;
 		case "19":
-			compare("Foglalt pályaelemre alagút építés.txt");
+			compare("19_Foglalt pályaelemre alagút építés.txt");
 			break;
 		case "20":
-			compare("Harmadik alagút építése nem történik meg.txt");
+			compare("20_Harmadik alagút építése nem történik meg.txt");
 			break;
 		case "21":
-			compare("Alagút építés nem történik meg, ha létezik egy másik alagút, ami éppen foglalt.txt");
+			compare("21_Alagút építés nem történik meg, ha létezik egy másik alagút, ami éppen foglalt.txt");
 			break;			
 		}
 		
